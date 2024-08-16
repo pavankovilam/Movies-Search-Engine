6c8cc96723a43f7931d060f81f93fafd66f01d02
Pavan Kumar Kovilampati <113405546+pavankovilam@users.noreply.github.com>
2024-08-16 17:24:37 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,32 @@
+let moviesContainer = document.getElementById("movies-displayer")
+var MovieName = document.getElementById("movie-name");
+var Loader = document.getElementById("status");
+
+ function load(){
+    event.preventDefault();
+    moviesContainer.innerHTML = ""
+    Loader.innerHTML = "<progress> </progress>"
+   let xhttp =  new XMLHttpRequest();
+   xhttp.onreadystatechange = function(){
+    if(this.readyState == 4){
+        Loader.innerHTML = ""
+        let result = JSON.parse(this.responseText);
+        console.log(result);
+        if( result.Response == "True" ){
+         let movies =  result.Search;
+         movies.map( (val,i) => {
+            moviesContainer.innerHTML += `<div>
+            <img src= ${val.Poster}/>
+            <p> <i> <b> Name :  </b> </i> ${val.Title} </p>
+            <p> <i> <b> Year : </b> </i> ${val.Year}  </p>
+            <p> <i> <b> Type : </b> </i> ${val.Type}  </p>
+            </div>`
+         } )
+        }else{
+            movieStatusElement.innerText =`No Results for ${MovieName.value}`
+        }
+    }
+   }
+   xhttp.open( "GET" , `https://www.omdbapi.com/?apikey=45f0782a&s=${MovieName.value}`  , true );
+   xhttp.send();
+} 
